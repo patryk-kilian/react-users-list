a4a71cda222964eef7e164c85013deed523acab9
patryk-kilian <patryk.kilian93@gmail.com>
2018-11-27 12:41:32 +0100

    Updates

---
+++ precache-manifest.8b6c88a582f3fa558ae9dddf21cc665c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8228e30c2af05adedb3b",
+    "url": "/react-users-list/static/js/runtime~main.8228e30c.js"
+  },
+  {
+    "revision": "74252cb14a250a9ce677",
+    "url": "/react-users-list/static/js/main.74252cb1.chunk.js"
+  },
+  {
+    "revision": "04b38b14fa0dc165c4b1",
+    "url": "/react-users-list/static/js/1.04b38b14.chunk.js"
+  },
+  {
+    "revision": "74252cb14a250a9ce677",
+    "url": "/react-users-list/static/css/main.ceb098df.chunk.css"
+  },
+  {
+    "revision": "afc1b56deac2ba07e2a7779540629c65",
+    "url": "/react-users-list/index.html"
+  }
+];
